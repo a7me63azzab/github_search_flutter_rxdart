e565cf1fab857674ac64def6e06f62666a3ebf74
Ahmed A. ElmenEm <devahmedazzab2014@gmail.com>
2018-11-18 13:01:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# github_search
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
+# github_search_flutter_rxdart
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:github_search/screens/search_screen.dart';
+import 'package:github_search/api/github_api.dart';
+
+
+void main(GithubApi api) {
+  runApp(SearchApp(api: api));
+}
+
+class SearchApp extends StatefulWidget {
+  final GithubApi api;
+
+  SearchApp({Key key, this.api}) : super(key: key);
+
+  @override
+  _RxDartGithubSearchAppState createState() => _RxDartGithubSearchAppState();
+}
+
+class _RxDartGithubSearchAppState extends State<SearchApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RxDart Github Search',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.grey,
+      ),
+      home: SearchScreen(api: widget.api),
+    );
+  }
+}
